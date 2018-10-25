ca2127b8722e9230a81c497f41dd4e657bf7635b
malli <malli@malli-PC>
2018-10-25 22:29:16 +0530

    1st change

---
+++ src/packa.java
@@ -0,0 +1,14 @@
+
+
+public class packa {
+
+	 packa()
+	{
+		System.out.println("fuck ass");
+	}
+public void setM()
+{
+	
+System.out.println("fuckass1");
+}
+}
---
+++ src/pck.java
@@ -0,0 +1,10 @@
+
+public class pck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	packa c=new packa();
+	c.setM();
+	}
+
+}
